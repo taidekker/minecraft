96503b8c1391f67eb076b33f65894f84b410c90d
taidekker <local_admin@localhost>
2021-06-18 15:04:09 -0700

    try1

---
+++ src/main/java/net/mcreator/tutorialmod/block/AdraniumBlockBlock.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.tutorialmod.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.tutorialmod.TutorialmodModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@TutorialmodModElements.ModElement.Tag
+public class AdraniumBlockBlock extends TutorialmodModElements.ModElement {
+	@ObjectHolder("tutorialmod:adranium_block")
+	public static final Block block = null;
+	public AdraniumBlockBlock(TutorialmodModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.IRON).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0).harvestLevel(1)
+					.harvestTool(ToolType.PICKAXE).setRequiresTool());
+			setRegistryName("adranium_block");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
